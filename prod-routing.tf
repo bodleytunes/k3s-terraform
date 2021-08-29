0531a10aae236c02304a96988c1d675867418d4f
Jon Clayton <m87tech@gmail.com>
2021-08-29 15:34:42 +0000

    subnet associated with worker route table

--- prod-routing.tf
+++ prod-routing.tf
@@ -19,3 +19,26 @@ resource "aws_main_route_table_association" "main" {
   vpc_id         = aws_vpc.prod.id
   route_table_id = aws_route_table.prod.id
 }
+
+resource "aws_route_table" "workers" {
+
+  vpc_id = aws_vpc.prod.id
+  
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.prod_nat_gateway.id
+  }
+
+    tags = {
+        Name = "workers"
+  }
+
+}
+
+
+resource "aws_route_table_association" "workers" {
+  for_each = aws_subnet.worker_subnets
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.workers.id
+}
